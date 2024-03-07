979089f6f1bce6e6c88120e29910d10c518de309
Ed Sorrie <ed_sorrie@hotmail.co.uk>
2024-03-07 15:00:17 +0000

    trying to display user info on profile page

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
-from flask import Flask, render_template, jsonify, request, redirect, url_for, flash
+from flask import Flask, render_template, jsonify, request, redirect, session, url_for, flash
 from pymongo import MongoClient
 
 app = Flask(__name__)
 app.secret_key = "2tc)(h@|HWT4=+8<:ZiUs;(fvd|8;u"
+
 client = MongoClient(host='project_mongodb', port=27017)
 db = client["project_mongodb"]
 users_collection = db['users']
@@ -35,19 +36,26 @@ def login():
         
         user = users_collection.find_one({'username': username, 'password': password})
         if user:
+            session['session_id'] = str(user['_id'])
             flash('Login Successful.', 'success')
+            return redirect(url_for('profile'))
         else:
             flash('Invalid username or password. Please try again.', 'danger')
     return render_template('login.html') 
 
 
 
-# @app.route('/user')
-# def fetch_user():
-#     db = get_db()
-#     _users = db.user_tb.find()
-#     users = [{"id": user["id"], "name": user["name"], "email": user["email"]} for user in _users]
-#     return jsonify({"users": users})
-
+@app.route('/profile')
+def profile():
+    if 'session_id' in session:
+        user = users_collection.find_one({'_id': session['session_id']})
+        if user:
+            return render_template('profile.html', user = user)
+        else:
+            return 'User not found', 404
+    else:
+        return 'Please log in to view profile', 401 
+        
+        
 if __name__ == '__main__': 
     app.run(host='0.0.0.0', debug=True) 
