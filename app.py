3d5043f9e965397c9072ad06fb69565997583aa6
Ed Sorrie <ed_sorrie@hotmail.co.uk>
2024-03-07 14:25:30 +0000

    register and user page routes setup, database accepts user form inputs and stores data

--- app.py
+++ app.py
@@ -1,26 +1,51 @@
-from flask import Flask, render_template, jsonify
-import pymongo 
+from flask import Flask, render_template, jsonify, request, redirect, url_for, flash
 from pymongo import MongoClient
 
 app = Flask(__name__)
-
-def get_db():
-    client = MongoClient(host='project_mongodb',
-                        port=27017,
-                        user='root',
-                        password='pass',
-                        authSource = 'admin')
-    db = client["user_db"]
-    return db
+app.secret_key = "2tc)(h@|HWT4=+8<:ZiUs;(fvd|8;u"
+client = MongoClient(host='project_mongodb', port=27017)
+db = client["project_mongodb"]
+users_collection = db['users']
 
 @app.route('/') 
 def index(): 
     return render_template('index.html') 
 
-# app.route('/user')
+@app.route('/register', methods=['GET', 'POST']) 
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        email = request.form['email']
+        password = request.form['password']
+        
+        if users_collection.find_one({'username': username}):
+            flash('Username already exists. Choose a different one.', 'danger')
+        else:
+            users_collection.insert_one({'username': username, 'password': password, 'email': email})
+            flash('Registration successful. You can now log in.', 'success')
+            return redirect(url_for('login'))
+
+    return render_template('register.html') 
+
+@app.route('/login', methods=['GET', 'POST']) 
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        
+        user = users_collection.find_one({'username': username, 'password': password})
+        if user:
+            flash('Login Successful.', 'success')
+        else:
+            flash('Invalid username or password. Please try again.', 'danger')
+    return render_template('login.html') 
+
+
+
+# @app.route('/user')
 # def fetch_user():
-#     db = get_db
-#     _users = db.user_db.find()
+#     db = get_db()
+#     _users = db.user_tb.find()
 #     users = [{"id": user["id"], "name": user["name"], "email": user["email"]} for user in _users]
 #     return jsonify({"users": users})
 
